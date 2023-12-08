0571a5551ba812857b47ac67f8a43b4b80511892
robledogar <robledogar@gmail.com>
2023-12-08 11:59:53 +0100

    Proyecto listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import Header from "./components/Header"
 import Button from "./components/Button"
+import { formatearDinero, calcularTotalPagar } from './helpers'
 
 
 
@@ -9,6 +10,23 @@ import Button from "./components/Button"
 function App() {
 
   const [cantidad, setCantidad] = useState(10000);
+  const [meses, setMeses] = useState(6);
+  const [total, setTotal] = useState(0);
+  const [pago, setPago] = useState(0)
+
+  useEffect(() => {
+    // console.log('Componente listo o la cantidad cambió');
+    const resultadoTotalPagar = calcularTotalPagar(cantidad, meses);
+    setTotal(resultadoTotalPagar);
+  }, [cantidad, meses]);
+
+
+  useEffect(() => {
+    //Calcular el pago mensual
+    setPago(total / meses)
+  }, [total])
+
+
 
   const MIN = 0;
   const MAX = 20000;
@@ -68,7 +86,33 @@ function App() {
           value={cantidad}
          />
 
-         <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">{cantidad}</p>
+         <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">
+          {formatearDinero(cantidad)}
+          </p>
+
+          <h2 className="text-2xl font-extrabold text-center text-gray-500" >
+            Elige un <span className="text-indigo-600">plazo</span> a pagar
+          </h2>
+
+          <select 
+            className="mt-5 w-full bg p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500"
+            value={meses}
+            onChange={ e => setMeses(+e.target.value)}
+          >
+            <option value="6">6 Meses</option>
+            <option value="12">12 Meses</option>
+            <option value="24">24 Meses</option>
+          </select>
+
+          <div className="my-5 space-y-3 bg-gray-50 p-5">
+            <h2 className="text-2xl font-extrabold text-center text-gray-500" >
+              Resumen <span className="text-indigo-600">de pagos</span>
+            </h2>
+            <p className="text-xl text-gray-500 text-center font-bold">{meses} Meses</p>
+            <p className="text-xl text-gray-500 text-center font-bold">{formatearDinero(total)} Total a pagar</p>
+            <p className="text-xl text-gray-500 text-center font-bold">{formatearDinero(pago)} Mensuales</p>
+            
+          </div>
         
     </div>
   )
